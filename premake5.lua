30fe40d551941a1b03961277f699a3b7b6cf6514
Daniel <danielga@outlook.pt>
2019-09-05 19:23:50 +0100

    Update README, .gitignore and premake5 project generator

--- premake5.lua
+++ premake5.lua
@@ -4,12 +4,9 @@ newoption({
 	value = "path to garrysmod_common directory"
 })
 
-local gmcommon = _OPTIONS.gmcommon or os.getenv("GARRYSMOD_COMMON")
-if gmcommon == nil then
-	error("you didn't provide a path to your garrysmod_common (https://github.com/danielga/garrysmod_common) directory")
-end
-
-include(gmcommon)
+local gmcommon = assert(_OPTIONS.gmcommon or os.getenv("GARRYSMOD_COMMON"),
+	"you didn't provide a path to your garrysmod_common (https://github.com/danielga/garrysmod_common) directory")
+include(path.join(gmcommon, "generator.v2.lua"))
 
 CreateWorkspace({name = "filesystem", abi_compatible = true})
 	CreateProject({serverside = true})
@@ -20,10 +17,10 @@ CreateWorkspace({name = "filesystem", abi_compatible = true})
 		IncludeSDKTier1()
 
 		filter("system:windows")
-			files({"../source/win32/*.cpp", "../source/win32/*.hpp"})
+			files({"source/win32/*.cpp", "source/win32/*.hpp"})
 
 		filter("system:linux or macosx")
-			files({"../source/posix/*.cpp", "../source/posix/*.hpp"})
+			files({"source/posix/*.cpp", "source/posix/*.hpp"})
 
 	CreateProject({serverside = false})
 		IncludeLuaShared()
@@ -32,7 +29,7 @@ CreateWorkspace({name = "filesystem", abi_compatible = true})
 		IncludeSDKTier1()
 
 		filter("system:windows")
-			files({"../source/win32/*.cpp", "../source/win32/*.hpp"})
+			files({"source/win32/*.cpp", "source/win32/*.hpp"})
 
 		filter("system:linux or macosx")
-			files({"../source/posix/*.cpp", "../source/posix/*.hpp"})
+			files({"source/posix/*.cpp", "source/posix/*.hpp"})
